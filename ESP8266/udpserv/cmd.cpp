bfdf3f4406f011ebbf5f546c703cfecf61a3bec6
alexander ershov <alexander.ershov@gmailcom>
2016-01-31 16:57:18 +0300

    c

--- ESP8266/udpserv/cmd.cpp
+++ ESP8266/udpserv/cmd.cpp
@@ -3,29 +3,79 @@
 
 #include "cmd.h"
 
-StaticJsonBuffer<200> jsonBuffer; // size - TBD
+int16_t _doCmd(JsonObject&,JsonObject&);
+void c_info(JsonObject&,JsonObject&);
+void c_reset(JsonObject&,JsonObject&);
 
+typedef void (*VFP)(JsonObject&,JsonObject&);
 
+const int BUF_SZ = 255; 
+const char *CMDS="I\0R\0";
+enum CMDS_ID {CMD_INFO=0, CMD_RESET=1, CMD_NOCMD=2};
+//void (*vf)()[2]={c_info, c_reset};
+
+VFP cmd_imp[2]={c_info, c_reset};
 
 // {"i":id,"c":"cmd","p1":param}
 // {"i":1,"c":"I"}
    
-int16_t CmdProc::doCmd(char *buf) {
+int16_t CmdProc::doCmd(char *buf) {  
   if(!buf) return 0;
+  char bufout[BUF_SZ];
+ {
+  StaticJsonBuffer<JSON_OBJECT_SIZE(4)> jsonBufferIn;
+  StaticJsonBuffer<JSON_OBJECT_SIZE(4)> jsonBufferOut;
+  JsonObject& root = jsonBufferIn.parseObject(buf);
+  JsonObject& rootOut = jsonBufferOut.createObject();
+  _doCmd(root, rootOut);
+  rootOut.printTo(Serial);
+  rootOut.printTo(bufout, 254);
+ }
+  strcpy(buf, bufout);     
+  return 0;
+}
 
-  JsonObject& root = jsonBuffer.parseObject(buf);
-
+int16_t _doCmd(JsonObject& root, JsonObject& rootOut) {  
   if (!root.success()) {
     Serial.println("parseObject() failed");
+    rootOut["i"] = -1;
+    rootOut["r"] = -1;
     return 0;
   }
 
   long id = root["i"];
   const char* cmd = root["c"];
-
+  
   Serial.print("Id: "); Serial.print(id);
-  if(cmd) Serial.print("Cmd:"); Serial.println(cmd);
-      
+  rootOut["i"] = id;
+  if(!cmd || !*cmd) {
+    rootOut["r"] = -2;
+    return 0;
+  }
+  if(cmd) {
+    Serial.print(" Cmd:"); Serial.println(cmd);
+    rootOut["r"] = 0;
+    rootOut["c"] = cmd;
+    const char *p=CMDS;
+    uint8_t i=0;
+    while(*p && strcmp(p, cmd)) { p+=strlen(p)+1; i++; }
+    if(i>=CMD_NOCMD) { rootOut["r"] = -2; return 0;}
+    (*(cmd_imp[i]))(root, rootOut);
+  }      
   return 0;
 }
+
+void c_info(JsonObject& root, JsonObject& rootOut) {
+  Serial.println("INFO"); 
+  rootOut["fhs"]=ESP.getFreeHeap();
+  rootOut["fss"]=ESP.getFreeSketchSpace();
+}
+
+void c_reset(JsonObject& root, JsonObject& rootOut) {
+  Serial.println("RST"); 
+  delay(1000);
+  ESP.restart();
+}
+
+
 
--- ESP8266/udpserv/udpserv.ino
+++ ESP8266/udpserv/udpserv.ino
@@ -3,15 +3,16 @@
 #include "cmd.h"
 
 void print_sys_info();
-void doCycle(uint16_t);
+void doCycle();
 
 const char* ssid = "NETGEAR";
 const char* password = "boarboar";
 const int udp_port = 4444;
 const int BUF_SZ = 255;
-const int CYCLE_TO = 10;
+const int CYCLE_TO = 2;
 bool isConnected=false;
 char packetBuffer[BUF_SZ];
+//char packetBufferOut[BUF_SZ];
 WiFiUDP udp_rcv;
 WiFiUDP udp_snd;
 uint32_t last_cycle;
@@ -28,8 +29,9 @@ void setup() {
   uint8_t i = 0;
   while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
   if(i == 21){
-    Serial.print("Could not connect to"); Serial.println(ssid);
-    while(1) delay(500);
+    Serial.print("Could not connect to "); Serial.println(ssid);
+    delay(10000);
+    ESP.reset();
   }
 
   if(udp_rcv.begin(udp_port)) {
@@ -54,11 +56,8 @@ void loop() {
       int len = udp_rcv.read(packetBuffer, BUF_SZ);
       if(len>BUF_SZ-1) len=BUF_SZ-1;
       packetBuffer[len] = 0;    
-      // echo
-      udp_snd.beginPacket(udp_rcv.remoteIP(), udp_rcv.remotePort());
-      udp_snd.write(packetBuffer, len);
-      udp_snd.endPacket();
-
+ 
+      doCycle(); yield();
       // dbg out
       uint32_t m2=millis();
       Serial.print("From: "); Serial.print(udp_rcv.remoteIP());
@@ -67,21 +66,26 @@ void loop() {
       Serial.print(" T: "); Serial.print(m2-m1);
       Serial.print(" Val: "); Serial.println(packetBuffer);
       //
-
+      doCycle(); yield();
+      
       cmd.doCmd(packetBuffer);
+
+     // resp
+      udp_snd.beginPacket(udp_rcv.remoteIP(), udp_rcv.remotePort());
+      udp_snd.write(packetBuffer, strlen(packetBuffer));
+      udp_snd.endPacket();
 
       print_sys_info();
     }
   }
-  uint32_t t = millis();
-  if(t-last_cycle >= CYCLE_TO) {
-    doCycle(t-last_cycle);
-    last_cycle = t;
-  }
-  //delay(10);
+  doCycle();
 }
 
-void doCycle(uint16_t dt) {
+void doCycle() {
+  uint32_t t = millis();
+  uint16_t dt=t-last_cycle;
+  if(dt < CYCLE_TO) return;
+  last_cycle = t;
   if(dt<=CYCLE_TO) cnt[0]++;
   else if(dt<=CYCLE_TO<<1) cnt[1]++;
   else if(dt<=CYCLE_TO<<2) cnt[2]++;
