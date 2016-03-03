9f390b1518fda8a971ebcbcfa14fa2766469bd3f
Alex Ershov <alex.ershov@gmail.com>
2016-03-03 15:44:28 +0300

    m

--- ESP8266/periph/master/master.ino
+++ ESP8266/periph/master/master.ino
@@ -10,14 +10,24 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Init Wire...");
 
-    Wire.begin(MPU_SDA, MPU_SDL);
-
-    Serial.println("Sending...");
-    bool res = I2Cdev::writeByte(DEV_ID, 0x01, 0x02);
-    Serial.println(res);
+    Wire.begin(MPU_SDA, MPU_SDL);    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  uint8_t buf[1];
+  delay(1000);
+  
+  Serial.println("Requesting...");
+  uint32_t t=millis();  
+  //bool res = I2Cdev::writeByte(DEV_ID, 0x01, 0x02);
+  bool res = I2Cdev::readByte(DEV_ID, 0x01, buf);
+  uint32_t dt=millis()-t;  
+  
+  Serial.print("R:\t");
+  Serial.print(res);
+  Serial.print("\tV:\t");
+  Serial.print(buf[0]);
+  Serial.print("\tT:\t");
+  Serial.print(dt);
+  Serial.println("ms");
 }
--- ESP8266/periph/slave/slave.ino
+++ ESP8266/periph/slave/slave.ino
@@ -12,44 +12,66 @@
 
 #include <Wire.h>
 
+uint32_t llast=0L;
+uint8_t cnt=0;
+
+uint8_t reg=0;
+
 void setup()
 {
-    delay(2000);
-    Serial.begin(9600);
-    Serial.println("Init Wire...");
-    
+  delay(2000);
+  Serial.begin(9600);
+  Serial.println("Init Wire...");    
   digitalWrite(RED_LED, LOW); 
   pinMode(RED_LED, OUTPUT);  
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
 }
 
 void loop()
 {
-  delay(100);
+  //delay(100);
+  if(llast && millis()-llast < 5000L) { // switch off LED
+    llast=0L;
+    digitalWrite(RED_LED, LOW);     
+  }
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
-  /*
-  while(1 < Wire.available()) // loop through all but the last
-  {
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
-  }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
-  */
-    Serial.print("Received...");
-    
+  
+  Serial.print("Received...REG=\t");    
+
+  reg=0;
+  if(Wire.available()) reg=Wire.read();    
+  Serial.print(reg, HEX);
+  Serial.print(",\tV=\t");
   while(Wire.available()) {
     char c = Wire.read();
     Serial.print(c, HEX);
+    Serial.print(' ');
   }
-  
   Serial.println();
+    
+  cnt++;
   
   digitalWrite(RED_LED, HIGH); 
+  llast=millis();
+}
+
+void requestEvent()
+{
+  Serial.println("Requested");    
+  /*
+  byte buffer[2];              // split int value into two bytes buffer
+  buffer[0] = returnValue >> 8;
+  buffer[1] = returnValue & 255;
+  Wire.write(buffer, 2);       // return response to last command
+  */
+  byte buffer[1];              // split int value into two bytes buffer
+  buffer[0] = cnt;
+  Wire.write(buffer, 1);       // return response to last command  
 }
