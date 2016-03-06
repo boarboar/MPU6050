aad4c94901f94372e30c35b7ad16f1aea958041e
alexander ershov <alexander.ershov@gmailcom>
2016-03-06 19:12:02 +0300

    i2c perif proto ok

--- ESP8266/periph/master/master.ino
+++ ESP8266/periph/master/master.ino
@@ -5,29 +5,49 @@ const int MPU_SDA=0;
 const int MPU_SDL=2;
 const int DEV_ID=4;
 
+
 void setup() {
-    delay(2000);
+//  digitalWrite(RED_LED, LOW); 
+//  pinMode(RED_LED, OUTPUT);
+  
+  delay(2000);
     Serial.begin(115200);
+    //Serial.begin(9600);
     Serial.println("Init Wire...");
 
-    Wire.begin(MPU_SDA, MPU_SDL);    
+    Wire.begin(MPU_SDA, MPU_SDL);
+
+//Wire.begin();
+
+Wire.setClock(100000L);
+
 }
 
+byte x = 0;
+
 void loop() {
-  uint8_t buf[1];
-  delay(1000);
+  uint8_t buf[2];
+  delay(500);
+//  digitalWrite(RED_LED, HIGH);
   
   Serial.println("Requesting...");
   uint32_t t=millis();  
-  //bool res = I2Cdev::writeByte(DEV_ID, 0x01, 0x02);
-  bool res = I2Cdev::readByte(DEV_ID, 0x01, buf);
+  //bool res = I2Cdev::writeByte(DEV_ID, 0xAB, x);
+  bool res = I2Cdev::readBytes(DEV_ID, 0xAB, 2, buf);
   uint32_t dt=millis()-t;  
   
   Serial.print("R:\t");
   Serial.print(res);
   Serial.print("\tV:\t");
-  Serial.print(buf[0]);
+  Serial.print(buf[0], HEX);
+  Serial.print(",");
+  Serial.print(buf[1], HEX);
   Serial.print("\tT:\t");
   Serial.print(dt);
   Serial.println("ms");
+  delay(500);
+//  digitalWrite(RED_LED, LOW);
+  
+  x++;
 }
+
--- ESP8266/periph/slave/slave.ino
+++ ESP8266/periph/slave/slave.ino
@@ -3,7 +3,7 @@
 
 // Demonstrates use of the Wire library
 // Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
+// Refer to the "Wire Master Writer" example for use with thisr
 
 // Created 29 March 2006
 
@@ -12,66 +12,96 @@
 
 #include <Wire.h>
 
+int rcv=0, req=0;
 uint32_t llast=0L;
 uint8_t cnt=0;
-
-uint8_t reg=0;
+uint8_t reg=0, val=0;
 
 void setup()
-{
-  delay(2000);
-  Serial.begin(9600);
-  Serial.println("Init Wire...");    
+{  
+ digitalWrite(P1_6, HIGH); 
+ //pinMode(P1_6, INPUT_PULLUP);  
+ digitalWrite(P1_7, HIGH); 
+ //pinMode(P1_7, INPUT_PULLUP);
+ 
+  digitalWrite(RED_LED, HIGH); 
+  pinMode(RED_LED, OUTPUT);  
+  
+  for(int i=0;i<5;i++) {
+    delay(100); 
+    digitalWrite(RED_LED, HIGH); 
+    delay(100);  
+    digitalWrite(RED_LED, LOW);
+  }
+  
+    delay(1000);
+    Serial.begin(9600);
+    Serial.println("Init Wire...");
+    
   digitalWrite(RED_LED, LOW); 
   pinMode(RED_LED, OUTPUT);  
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
   Wire.onRequest(requestEvent); // register event
+
+  llast=millis();
+
 }
+
 
 void loop()
 {
-  //delay(100);
-  if(llast && millis()-llast < 5000L) { // switch off LED
+  if(rcv>0 || req>0) {
+    if(rcv>0) {
+    Serial.print("Rcv ");
+    Serial.print(cnt);
+    Serial.print("\tBytes=\t");
+    Serial.print(rcv);
+    Serial.print("\tReg=\t");
+    Serial.print(reg, HEX);
+    Serial.print("\tVal=\t");
+    Serial.print(val, HEX);
+    Serial.println();
+    rcv=0;
+    }
+    if(req>0) {
+      Serial.println("Req");
+      req=0;
+    }
+    digitalWrite(RED_LED, HIGH); 
+    llast=millis();  
+   
+  } else if(llast && millis()-llast > 5000L) { // switch off LED
     llast=0L;
     digitalWrite(RED_LED, LOW);     
-  }
+  }  
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany)
 {
-  
-  Serial.print("Received...REG=\t");    
-
   reg=0;
-  if(Wire.available()) reg=Wire.read();    
-  Serial.print(reg, HEX);
-  Serial.print(",\tV=\t");
-  while(Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c, HEX);
-    Serial.print(' ');
+  int i=0;
+  while(Wire.available()>0) {
+    int c = Wire.read();
+    if(i==0) reg=(int)c;
+    else if(i==1) val=(int)c;
+    i++;
   }
-  Serial.println();
-    
+  rcv=i;
   cnt++;
-  
-  digitalWrite(RED_LED, HIGH); 
-  llast=millis();
 }
 
 void requestEvent()
 {
-  Serial.println("Requested");    
-  /*
+  req=1;  
   byte buffer[2];              // split int value into two bytes buffer
-  buffer[0] = returnValue >> 8;
-  buffer[1] = returnValue & 255;
-  Wire.write(buffer, 2);       // return response to last command
-  */
-  byte buffer[1];              // split int value into two bytes buffer
-  buffer[0] = cnt;
-  Wire.write(buffer, 1);       // return response to last command  
+  //buffer[0] = returnValue >> 8;
+  //buffer[1] = returnValue & 255;
+  buffer[0] = reg;
+  buffer[1] = cnt;
+  Wire.write(buffer, 2);       // return response to last command  
 }
+
+
