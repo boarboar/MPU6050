205e09544cec56728cbeb4b877784b00295c62a3
alexander.ershov <alexander.ershov@gmail.com>
2017-02-02 20:22:45 +0300

    cam fix

--- ESP8266/console/camera.py
+++ ESP8266/console/camera.py
@@ -4,7 +4,7 @@ import wx.html2
 class CameraPanel(wx.Window):
     " camera panel"
     def __init__(self, parent):
-        wx.Window.__init__(self, parent, wx.ID_ANY, style=wx.SIMPLE_BORDER, size=(320,240))
+        wx.Window.__init__(self, parent, wx.ID_ANY, style=wx.SIMPLE_BORDER, size=(160,120))
         self.SetBackgroundColour('BLACK')
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.browser = wx.html2.WebView.New(self)
@@ -12,7 +12,7 @@ class CameraPanel(wx.Window):
         self.SetSizer(sizer)
         #self.browser.LoadURL("http://www.google.com")
         #self.browser.LoadURL("http://192.168.1.120:8080/")
-        self.browser.LoadURL("http:root@tombaz@//192.168.1.120/")
+        self.browser.LoadURL("http://root:tombaz@192.168.1.120/")
 
 
         #self.browser.LoadURL("http://192.168.1.1/")
